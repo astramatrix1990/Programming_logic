66bf9c78afebd928f2f3945c939669fbf2973829
astramatrix <astradeveloper1990@doisirmaos.com>
2021-05-10 16:00:11 -0300

    Main C

---
+++ c/main.c
@@ -0,0 +1,11 @@
+//Bibliotecas importadas no programa.
+#include <stdio.h>
+#include <stdlib.h>
+
+//Função principal Main do programa em C.
+int main(){ 
+  //Função que imprime uma mensagem na tela, o contra barra n é um pula linha.
+  printf("Olá Mundo em C\n");
+  return 0; 
+  //A função int vai retornar 0;
+}
